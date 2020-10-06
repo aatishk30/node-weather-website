75ed080ed8185e54501331557a155718668744db
aatishk30 <aatishk30@gmal.com>
2020-10-07 00:22:41 +0530

    removed a commented line

--- public/js/app.js
+++ public/js/app.js
@@ -23,7 +23,6 @@ weatherForm.addEventListener('submit',(e)=>{
 
     const location =search.value
     //console.log(location);
-    //console.log(location);
 
     fetch('/weather?address='+location).then((response)=>{
 
